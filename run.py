9b850768226ce5d99875918c15eb5a2207cb5d3b
matzhedman <hedmanmatz@gmail.com>
2023-02-01 19:12:54 +0000

    Add content to MAIN MENU with three choices

--- run.py
+++ run.py
@@ -47,5 +47,26 @@ def mainMenu():
     print(x.strftime("%x"))
     print('____________\n')
 
+    print('MAIN MENU \n')
+    print('1) Book a Time')
+    print('2) Show all current Bookings')
+    print('00) Back to Start\n')
+    while True:
+        try:
+            choice = int(input('Type in number to choose an option: \n'))
+        except ValueError:
+            print("Not a valid number")
+
+        if choice == 1:
+            choose_a_day()
+            break
+        elif choice == 2:
+            show_my_booking()
+            break
+        elif choice == 00:
+            log_out()
+        else:
+            print('Not a valid number, please choose 1), 2) or 00).')
+
 start_up = start()
 menu = mainMenu()
